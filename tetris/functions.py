efed8b7a026ee26d9c2f98ef8813db5e9c80b5ba
Willian <willianbrtl@hotmail.com>
2023-01-28 12:02:39 -0300

    Preparando variáveis e funções

---
+++ tetris/functions.py
@@ -0,0 +1,34 @@
+# functions
+# - create_grid
+# - draw_grid
+# - draw_window
+
+def create_grid(locked_positions={}):
+    pass
+
+def convert_shape_format(shape):
+    pass
+
+def valid_space(shape, grid):
+    pass
+
+def check_lost(positions):
+    pass
+
+def get_shape():
+    pass
+
+def draw_text_middle(text, size, color, surface):  
+    pass
+   
+def draw_grid(surface, row, col):
+    pass
+
+def clear_rows(grid, locked):
+    pass
+
+def draw_next_shape(shape, surface):
+    pass
+
+def draw_window(surface):
+    pass
--- tetris/tetris_main.py
+++ tetris/tetris_main.py
@@ -1,30 +1,67 @@
 import pygame
 from pygame.locals import *
 from sys import exit
-from colors import *
 import os
 
-root = os.path.dirname(__file__)   
-# musica = os.path.join(root, 'arquivos/nome_arquivo.wav')
+from colors import *
+from shapes import *  # S, Z, I, O, J, L, T
+from functions import *
+
+# creating the data structure for pieces
+# setting up global vars
+# functions
+# - rotating shape in main
+# - setting up the main
+
+"""
+10 x 20 square grid
+shapes: S, Z, I, O, J, L, T
+represented in order by 0 - 6
+"""
 
 # Iniciando Pygame
 pygame.init()
+pygame.font.init()
 
-LARGURA = 980
-ALTURA = 550
-FPS = 60
+# GLOBALS VARS
+root = os.path.dirname(__file__)   
+# musica = os.path.join(root, 'arquivos/nome_arquivo.wav')
 
-tela = pygame.display.set_mode((LARGURA, ALTURA))
-pygame.display.set_caption('Tetris')
+LARGURA_TELA = 800
+ALTURA_TELA = 700
+GRID_WIDTH = 300  # meaning 300 // 10 = 30 width per block
+GRID_HEIGHT = 600  # meaning 600 // 20 = 30 height per block
+BLOCK_SIZE = 30
+CLOCK_FPS = 60
 
-clock = pygame.time.Clock()
-while True:  # Loop principal
-    clock.tick(FPS)
-    tela.fill(black)
+TOP_LEFT_X = (LARGURA_TELA - GRID_WIDTH) // 2
+TOP_LEFT_Y = ALTURA_TELA - GRID_HEIGHT
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-    
-    pygame.display.update()
+pieces = {'shapes': [S, Z, I, O, J, L, T],
+          'colors': [green, red, cyan, yellow, orange, blue, purple]}
+# index 0 - 6 represent shape
+
+class Piece(object):
+    pass
+
+def main():
+    tela = pygame.display.set_mode((LARGURA_TELA, ALTURA_TELA))
+    pygame.display.set_caption('Tetris')
+
+    clock = pygame.time.Clock()
+    while True:  # Loop principal
+        clock.tick(CLOCK_FPS)
+        tela.fill(black)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        
+        pygame.display.update()
+
+def main_menu():
+    main()
+
+if __name__ == '__main__': # start game
+    main_menu()
